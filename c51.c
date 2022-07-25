e745fef2f2d62b3a2fd9fb524eb72443ccb05685
lijienihao <1335764921@qq.com>
2022-07-25 19:53:57 +0800

    again test

---
+++ c51.c
@@ -0,0 +1,28 @@
+//#include "stdio.h"
+
+#define uchar unsigned char
+#define uint unsigned int
+
+void delay_1ms(uint n)
+{
+    //for(i = 0;i < n;i++)
+
+}
+
+void uartinit(void)
+{
+   // SON = 0x02;
+}
+
+//void uart(void) interrupt 4
+//{
+   // EA = 0;
+//}
+
+void main(void)
+{
+    while(1)
+    {
+
+    }
+}
